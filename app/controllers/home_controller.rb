b8e5194ea8fad1128210cc80afc4291a362fbbdf
miyasaka <guleguleru@gmail.com>
2024-07-15 16:44:04 +0900

    bodyの左右分割をhome#topにのみ適用

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
+  before_action :set_layout, only: [:top]
+
   def top
   end
 
   def cookrice
+  end
+
+  private
+
+  def set_layout
+    @use_split_layout = true
   end
 end
