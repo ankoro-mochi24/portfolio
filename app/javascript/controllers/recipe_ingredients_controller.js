d957014e6987992e79e90dbd192ba8d023fc6751
miyasaka <guleguleru@gmail.com>
2024-07-22 16:07:16 +0900

    フォーム編集-材料の複数同時投稿/編集

---
+++ app/javascript/controllers/recipe_ingredients_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="recipe-ingredients"
+export default class extends Controller {
+  static targets = ["container", "template"]
+
+  addField(event) {
+    event.preventDefault()
+    const content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
+    this.containerTarget.insertAdjacentHTML("beforeend", content)
+  }
+
+  removeField(event) {
+    event.preventDefault()
+    const field = event.target.closest(".nested-fields")
+    if (this.containerTarget.childElementCount > 1) {
+      if (field.dataset.newRecord == "true") {
+        field.remove()
+      } else {
+        field.querySelector("input[name*='_destroy']").value = 1
+        field.style.display = "none"
+      }
+    }
+  }
+}
