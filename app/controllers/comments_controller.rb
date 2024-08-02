b412829d0b9131b0d947db2d5fd8337dee15bc52
miyasaka <guleguleru@gmail.com>
2024-08-02 14:30:18 +0900

    コメントコントローラーのコード修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_commentable, only: [:create]
   before_action :set_comment, only: [:update, :destroy]
+  before_action :authorize_user, only: [:update, :destroy]
 
   def create
     @comment = @commentable.comments.build(comment_params)
@@ -14,7 +15,7 @@ class CommentsController < ApplicationController
     else
       respond_to do |format|
         format.html { redirect_back fallback_location: root_path, alert: 'コメントの追加に失敗しました。' }
-        #format.turbo_stream { render :error }
+        format.turbo_stream { render :error }
       end
     end
   end
@@ -28,7 +29,7 @@ class CommentsController < ApplicationController
     else
       respond_to do |format|
         format.html { redirect_back fallback_location: root_path, alert: 'コメントの更新に失敗しました。' }
-        #format.turbo_stream { render :error }
+        format.turbo_stream { render :error }
       end
     end
   end
@@ -45,13 +46,24 @@ class CommentsController < ApplicationController
 
   def set_commentable
     @commentable = params[:commentable_type].constantize.find(params[:commentable_id])
+  rescue NameError, ActiveRecord::RecordNotFound => e
+    Rails.logger.warn("不正なリクエスト: #{e.message}")
+    redirect_back fallback_location: root_path, alert: '不正なリクエストです。'
   end
 
   def set_comment
     @comment = Comment.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_back fallback_location: root_path, alert: 'コメントが見つかりません。'
+  end
+
+  def authorize_user
+    unless @comment.user == current_user
+      redirect_back fallback_location: root_path, alert: '他のユーザーのコメントを編集・削除することはできません。'
+    end
   end
 
   def comment_params
-    params.require(:comment).permit(:body, :commentable_type, :commentable_id)
+    params.require(:comment).permit(:body)
   end
 end
