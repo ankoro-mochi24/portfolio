c82a623ec2022a91994b8d275ce34ab065302862
miyasaka <guleguleru@gmail.com>
2024-08-04 17:11:11 +0900

    アクション実装-goodとbatの同時ON不可に

--- app/controllers/user_actions_controller.rb
+++ app/controllers/user_actions_controller.rb
@@ -1,21 +1,18 @@
 class UserActionsController < ApplicationController
-  include ActionView::RecordIdentifier # 追加
+  include ActionView::RecordIdentifier
   before_action :authenticate_user!
 
   def create
     @user_action = current_user.user_actions.new(user_action_params)
+    remove_opposite_action(@user_action)
     if @user_action.save
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace("#{dom_id(@user_action.actionable)}_#{@user_action.action_type}_actions", partial: 'shared/user_action', locals: { actionable: @user_action.actionable, action_type: @user_action.action_type })
-        end
+        format.turbo_stream
         format.html { redirect_to @user_action.actionable, notice: "#{@user_action.action_type} added." }
       end
     else
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace("#{dom_id(@user_action.actionable)}_#{@user_action.action_type}_actions", partial: 'shared/user_action', locals: { actionable: @user_action.actionable, action_type: @user_action.action_type })
-        end
+        format.turbo_stream
         format.html { redirect_to @user_action.actionable, alert: "Unable to perform this action." }
       end
     end
@@ -24,13 +21,11 @@ class UserActionsController < ApplicationController
   def destroy
     @user_action = current_user.user_actions.find(params[:id])
     @actionable = @user_action.actionable
-    action_type = @user_action.action_type
+    @action_type = @user_action.action_type
     @user_action.destroy
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("#{dom_id(@actionable)}_#{action_type}_actions", partial: 'shared/user_action', locals: { actionable: @actionable, action_type: action_type })
-      end
-      format.html { redirect_to @actionable, notice: "#{action_type} removed." }
+      format.turbo_stream
+      format.html { redirect_to @actionable, notice: "#{@action_type} removed." }
     end
   end
 
@@ -38,5 +33,11 @@ class UserActionsController < ApplicationController
 
   def user_action_params
     params.require(:user_action).permit(:action_type, :actionable_type, :actionable_id)
+  end
+
+  def remove_opposite_action(user_action)
+    opposite_action_type = user_action.action_type == 'good' ? 'bat' : 'good'
+    opposite_action = current_user.user_actions.find_by(actionable: user_action.actionable, action_type: opposite_action_type)
+    opposite_action&.destroy
   end
 end
