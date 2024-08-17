9fe6ced03c175f6d53b970479d0e7a75b9398d04
miyasaka <guleguleru@gmail.com>
2024-08-17 15:24:41 +0900

    マイページ-調理器具のフォームの動的追加削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,45 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_layout
-  
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_layout, only: [:show, :edit]
+
   def show
     @user = User.find(params[:id])
     @kitchen_tools = @user.kitchen_tools
   end
 
   def edit
-    @user = User.find(params[:id])
-    @kitchen_tools = KitchenTool.all
+    @user.user_kitchen_tools.each do |ukt|
+      ukt.kitchen_tool_name = ukt.kitchen_tool.name if ukt.kitchen_tool.present?
+    end
+    @user.user_kitchen_tools.build if @user.user_kitchen_tools.empty?
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
+    @user.assign_attributes(user_params)
+
+    @user.user_kitchen_tools.each do |tool|
+      tool.mark_for_destruction if tool._destroy == "1"
+    end
+
+    if @user.save
       redirect_to user_path(@user), notice: 'プロフィールが更新されました。'
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def set_layout
-    @use_sidebar = true # ここでサイドバー用のフラグを設定
+    @use_sidebar = true
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, user_kitchen_tools_attributes: [:id, :kitchen_tool_id, :_destroy])
+    params.require(:user).permit(:name, :email, user_kitchen_tools_attributes: [:id, :kitchen_tool_id, :kitchen_tool_name, :_destroy])
   end
 end
--- app/models/user_kitchen_tool.rb
+++ app/models/user_kitchen_tool.rb
@@ -2,5 +2,7 @@ class UserKitchenTool < ApplicationRecord
   belongs_to :user
   belongs_to :kitchen_tool
 
+  attr_accessor :kitchen_tool_name
+
   validates :user_id, uniqueness: { scope: :kitchen_tool_id }
 end
