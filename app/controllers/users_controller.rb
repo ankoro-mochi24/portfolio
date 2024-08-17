2b143cc708cc827ebd25b2d301d84a6b7a5f07c4
miyasaka <guleguleru@gmail.com>
2024-08-17 11:12:25 +0900

    サイドバーの部分テンプレート化とマイページedit画面のひな形実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_layout
+  
   def show
     @user = User.find(params[:id])
     @kitchen_tools = @user.kitchen_tools
-    @recipes = @user.recipes
-    @foodstuffs = @user.foodstuffs
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @kitchen_tools = KitchenTool.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'プロフィールが更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_layout
     @use_sidebar = true # ここでサイドバー用のフラグを設定
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, user_kitchen_tools_attributes: [:id, :kitchen_tool_id, :_destroy])
   end
 end
