e2850f7d5a64044aac1f7ac9639b8b2b6162f481
miyasaka <guleguleru@gmail.com>
2024-08-23 16:35:31 +0900

    top サイドバーで絞込実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,18 @@ class HomeController < ApplicationController
   def top
     if params[:query].present?
       query = params[:query]
-      @recipes = Recipe.search(query).page(params[:page]).per(10)
-      @foodstuffs = Foodstuff.search(query).page(params[:page]).per(10)
+      @recipes = Recipe.search(query)
+      @foodstuffs = Foodstuff.search(query)
     else
-      @recipes = Recipe.page(params[:page]).per(10)
-      @foodstuffs = Foodstuff.page(params[:page]).per(10)
+      @recipes = Recipe.all
+      @foodstuffs = Foodstuff.all
     end
   
+    filter_content if user_signed_in? && params[:filter].present?
+
+    @recipes = @recipes.page(params[:page]).per(10) if @recipes.present?
+    @foodstuffs = @foodstuffs.page(params[:page]).per(10) if @foodstuffs.present?
+
     case @view
     when 'recipes'
       @foodstuffs = nil
@@ -23,5 +28,19 @@ class HomeController < ApplicationController
 
   def set_view
     @view = params[:view] || 'both'
+  end
+
+  def filter_content
+    case params[:filter]
+    when 'bookmarks'
+      @recipes = @recipes.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bookmark', actionable_type: 'Recipe' })
+      @foodstuffs = @foodstuffs.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bookmark', actionable_type: 'Foodstuff' })
+    when 'good'
+      @recipes = @recipes.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'good', actionable_type: 'Recipe' })
+      @foodstuffs = @foodstuffs.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'good', actionable_type: 'Foodstuff' })
+    when 'bad'
+      @recipes = @recipes.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bad', actionable_type: 'Recipe' })
+      @foodstuffs = @foodstuffs.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bad', actionable_type: 'Foodstuff' })
+    end
   end
 end
