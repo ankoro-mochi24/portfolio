06dd3d0ed96150a64afa48d5698057742e5ad211
miyasaka <guleguleru@gmail.com>
2024-08-23 15:21:52 +0900

    topのhtmlの調整

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
-  before_action :set_layout, only: [:top]
   before_action :set_view, only: [:top]
-  before_action :show_tabs, only: [:top]
 
   def top
     if params[:query].present?
@@ -23,15 +21,7 @@ class HomeController < ApplicationController
 
   private
 
-  def set_layout
-    @use_split_layout = true
-  end
-
   def set_view
     @view = params[:view] || 'both'
-  end
-
-  def show_tabs
-    @show_tabs = true
   end
 end
