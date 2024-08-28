1a46898954ed605b3883569727f500dc4e40ae3b
miyasaka <guleguleru@gmail.com>
2024-08-28 14:12:55 +0900

    TOPサイドバー アクションチェックに応じてヒット件数を更新

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,10 +10,10 @@ class HomeController < ApplicationController
       @recipes = Recipe.includes(:user_actions).page(params[:page]).per(10)
       @foodstuffs = Foodstuff.includes(:user_actions).page(params[:page]).per(10)
     end
-  
+
     filter_content if user_signed_in? && params[:filter].present?
     set_filter_counts if user_signed_in?
-  
+
     case @view
     when 'recipes'
       @foodstuffs = nil
@@ -21,7 +21,7 @@ class HomeController < ApplicationController
       @recipes = nil
     end
   end
-  
+
   private
 
   def set_view
@@ -40,28 +40,8 @@ class HomeController < ApplicationController
       @recipes = @recipes.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bad', actionable_type: 'Recipe' })
       @foodstuffs = @foodstuffs.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bad', actionable_type: 'Foodstuff' })
     when 'kitchen_tools'
-      # 持っている調理器具で作れるレシピに絞り込み
       @recipes = @recipes.joins(:kitchen_tools).where(kitchen_tools: { id: current_user.kitchen_tools.pluck(:id) })
-      @foodstuffs = nil # 食品は関連しないのでnilに設定
+      @foodstuffs = nil
     end
-  end
-
-  def set_filter_counts
-    @bookmark_count = {
-      recipes: Recipe.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bookmark', actionable_type: 'Recipe' }).count,
-      foodstuffs: Foodstuff.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bookmark', actionable_type: 'Foodstuff' }).count
-    }
-    
-    @good_count = {
-      recipes: Recipe.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'good', actionable_type: 'Recipe' }).count,
-      foodstuffs: Foodstuff.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'good', actionable_type: 'Foodstuff' }).count
-    }
-    
-    @bad_count = {
-      recipes: Recipe.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bad', actionable_type: 'Recipe' }).count,
-      foodstuffs: Foodstuff.joins(:user_actions).where(user_actions: { user_id: current_user.id, action_type: 'bad', actionable_type: 'Foodstuff' }).count
-    }
-
-    @kitchen_tools_count = Recipe.joins(:kitchen_tools).where(kitchen_tools: { id: current_user.kitchen_tools.pluck(:id) }).count
   end
 end
