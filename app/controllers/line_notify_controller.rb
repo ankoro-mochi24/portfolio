df9f530b8e627caca1026cedc978c7ed59504c12
miyasaka <guleguleru@gmail.com>
2024-09-13 16:16:50 +0900

    LineNotifyの連携解除時通知実装

--- app/controllers/line_notify_controller.rb
+++ app/controllers/line_notify_controller.rb
@@ -32,9 +32,13 @@ class LineNotifyController < ApplicationController
       redirect_to profile_path, alert: "LINE Notifyとの連携に失敗しました。"
     end
   end
-  
+
   def unlink
-    current_user.update(line_notify_token: nil)
+    if current_user.line_notify_token.present?
+      LineNotifyService.new(current_user.line_notify_token).send_notification("LINE Notifyの連携を解除しました。")
+      current_user.update(line_notify_token: nil)
+    end
+    
     redirect_to profile_path, notice: "LINE Notifyの連携を解除しました。"
   end
 end
