d7d86c27fa31ea573f4a5f07d4f29318f5ce4a08
miyasaka <guleguleru@gmail.com>
2024-09-14 09:47:12 +0900

    Update Elasticsearch configuration for Heroku

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 Searchkick.client = Elasticsearch::Client.new(
-  hosts: ["http://elasticsearch:9200"],  # Elasticsearchのホスト設定
+  hosts: [ENV['SEARCHBOX_URL'] || 'http://localhost:9200'],  # HerokuのURLがない場合はローカルElasticsearchを使用
   retry_on_failure: true,  # 失敗時のリトライを有効にする
   transport_options: { request: { timeout: 250 } }  # リクエストのタイムアウトを250秒に設定
 )
