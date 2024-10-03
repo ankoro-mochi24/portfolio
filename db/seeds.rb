5d59808c6e5a46eb08cf43807ecb60267124034a
miyasaka <guleguleru@gmail.com>
2024-10-03 13:16:45 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,67 +1,17 @@
 require 'faker'
 
 # ユーザーのサンプルデータを作成（ユーザーがいない場合のため）
-5.times do
-  User.create(
-    email: Faker::Internet.email,
-    password: 'password',
-    password_confirmation: 'password',
-    name: Faker::Name.name
-  )
-end
-
-# レシピのサンプルデータを作成
 10.times do
-  recipe = Recipe.new(
-    title: Faker::Food.dish,
-    user_id: User.pluck(:id).sample
+  User.create!(
+    name: Faker::Name.name,                  # ランダムな名前を生成
+    email: Faker::Internet.unique.email,      # ランダムな一意のメールアドレスを生成
+    password: 'password',                     # デフォルトのパスワードを設定
+    password_confirmation: 'password',        # パスワード確認
+    created_at: Faker::Date.between(from: 2.years.ago, to: Date.today),  # 過去2年間でランダムな作成日
+    updated_at: Faker::Date.between(from: 2.years.ago, to: Date.today)   # 過去2年間でランダムな更新日
   )
-  # S3の画像URLを指定
-  recipe.dish_image = 'https://okome-biyori-bucket.s3.ap-northeast-1.amazonaws.com/sample.jpg'
-
-  # 材料を追加
-  3.times do
-    ingredient = Ingredient.find_or_create_by(name: Faker::Food.ingredient)
-    recipe.recipe_ingredients.build(ingredient: ingredient)
-  end
-
-  # 調理器具を追加
-  3.times do
-    kitchen_tool = KitchenTool.find_or_create_by(name: Faker::Appliance.equipment)
-    recipe.recipe_kitchen_tools.build(kitchen_tool: kitchen_tool, kitchen_tool_name: kitchen_tool.name)
-  end
-
-  if recipe.save
-    # レシピステップを追加
-    3.times do
-      step = recipe.recipe_steps.build(
-        text: Faker::Food.description
-      )
-      # S3の画像URLを指定
-      step.step_image = 'https://okome-biyori-bucket.s3.ap-northeast-1.amazonaws.com/sample.jpg'
-      step.save
-    end
-  else
-    puts "Recipe creation failed: #{recipe.errors.full_messages.join(", ")}"
-  end
 end
 
-# 食品のサンプルデータを作成
-30.times do
-  foodstuff = Foodstuff.new(
-    name: Faker::Food.ingredient,
-    price: Faker::Commerce.price(range: 1..100.0).to_i, # 価格を整数に変換
-    description: Faker::Food.description,
-    link: Faker::Internet.url,
-    user_id: User.pluck(:id).sample,
-    # S3の画像URLを指定
-    image: 'https://okome-biyori-bucket.s3.ap-northeast-1.amazonaws.com/sample.jpg'
-  )
-
-  if foodstuff.save
-  else
-    puts "Foodstuff creation failed: #{foodstuff.errors.full_messages.join(", ")}"
-  end
-end
-
+=begin
+=end
 puts "サンプルデータの作成が完了しました！"
