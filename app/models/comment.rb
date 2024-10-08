e347d8f7e05ea372af556b64d69769fff78bdb6e
miyasaka <guleguleru@gmail.com>
2024-10-08 16:52:54 +0900

    コメントテスト修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 1, maximum: 400 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Comment, type: :model do
   it "bodyがあり、userおよびcommentableが関連付けられている場合、有効である" do
     user = FactoryBot.create(:user)
     recipe = FactoryBot.create(:recipe, user: user)  # CommentableはRecipeにしてテスト
-    comment = Comment.new(body: "コメントです", user: user, commentable: recipe)
+    comment = Comment.new(body: "これはテストコメントです。", user: user, commentable: recipe)
     expect(comment).to be_valid
   end
 
@@ -16,19 +16,59 @@ RSpec.describe Comment, type: :model do
     expect(comment).not_to be_valid
   end
 
+  it "bodyが最小文字数未満の場合は無効である" do
+    user = FactoryBot.create(:user)
+    recipe = FactoryBot.create(:recipe, user: user)
+    comment = Comment.new(body: "", user: user, commentable: recipe)
+    expect(comment).not_to be_valid
+  end
+
+  it "bodyが最大文字数を超える場合は無効である" do
+    user = FactoryBot.create(:user)
+    recipe = FactoryBot.create(:recipe, user: user)
+    long_body = "あ" * 401  # 400文字を超える
+    comment = Comment.new(body: long_body, user: user, commentable: recipe)
+    expect(comment).not_to be_valid
+  end
+
   it "userがない場合は無効である" do
     recipe = FactoryBot.create(:recipe)
-    comment = Comment.new(body: "コメントです", user: nil, commentable: recipe)
+    comment = Comment.new(body: "これはテストコメントです。", user: nil, commentable: recipe)
     expect(comment).not_to be_valid
   end
 
   it "commentableがない場合は無効である" do
     user = FactoryBot.create(:user)
-    comment = Comment.new(body: "コメントです", user: user, commentable: nil)
+    comment = Comment.new(body: "これはテストコメントです。", user: user, commentable: nil)
     expect(comment).not_to be_valid
   end
 
   # アソシエーションのテスト
   it { should belong_to(:user) }
-  it { should belong_to(:commentable) }
+
+  it "commentableがポリモーフィックであることを確認する" do
+    comment = Comment.reflect_on_association(:commentable)
+    expect(comment.options[:polymorphic]).to be_truthy
+  end
+
+  # 削除時のテスト
+  it "userが削除されたときにコメントも削除される" do
+    user = FactoryBot.create(:user)
+    recipe = FactoryBot.create(:recipe, user: user)
+    comment = FactoryBot.create(:comment, user: user, commentable: recipe)
+    
+    expect { user.destroy }.to change { Comment.count }.by(-1)
+  end
+
+  it "commentable（例: recipe）が削除されたときにコメントも削除される" do
+    user = FactoryBot.create(:user)
+    recipe = FactoryBot.create(:recipe, user: user)
+    comment = FactoryBot.create(:comment, user: user, commentable: recipe)
+    
+    expect { recipe.destroy }.to change { Comment.count }.by(-1)
+  end
+
+  # データベースインデックスのテスト
+  it { should have_db_index([:commentable_type, :commentable_id]) }
+  it { should have_db_index(:user_id) }
 end
