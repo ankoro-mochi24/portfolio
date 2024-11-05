b2a9b90efa8ed473dc5cee3ef90f38d8fcd8eb80
miyasaka <guleguleru@gmail.com>
2024-11-05 10:16:28 +0900

    foodstuff_spec.rbを修正

--- spec/models/foodstuff_spec.rb
+++ spec/models/foodstuff_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Foodstuff, type: :model do
 
   # バリデーションのテスト
   describe 'バリデーションのテスト' do
-    it 'すべての属性が存在する場合、有効である' do
+    it 'すべての属性が有効である場合、有効である' do
       foodstuff = FactoryBot.build(:foodstuff, user: user)
       expect(foodstuff).to be_valid
     end
@@ -13,37 +13,37 @@ RSpec.describe Foodstuff, type: :model do
     it 'nameがない場合、無効である' do
       foodstuff = FactoryBot.build(:foodstuff, name: nil, user: user)
       expect(foodstuff).not_to be_valid
-      expect(foodstuff.errors[:name]).to include("名前を入力してください")
+      expect(foodstuff.errors[:name]).to include(I18n.t('activerecord.errors.models.foodstuff.attributes.name.blank'))
     end
 
     it 'priceがない場合、無効である' do
       foodstuff = FactoryBot.build(:foodstuff, price: nil, user: user)
       expect(foodstuff).not_to be_valid
-      expect(foodstuff.errors[:price]).to include("価格を入力してください")
+      expect(foodstuff.errors[:price]).to include(I18n.t('activerecord.errors.models.foodstuff.attributes.price.blank'))
     end
 
     it 'priceが負の値の場合、無効である' do
       foodstuff = FactoryBot.build(:foodstuff, price: -1, user: user)
       expect(foodstuff).not_to be_valid
-      expect(foodstuff.errors[:price]).to include("価格は0以上の値にしてください")
+      expect(foodstuff.errors[:price]).to include(I18n.t('activerecord.errors.models.foodstuff.attributes.price.greater_than_or_equal_to'))
     end
 
     it 'priceが整数でない場合、無効である' do
       foodstuff = FactoryBot.build(:foodstuff, price: 10.5, user: user)
       expect(foodstuff).not_to be_valid
-      expect(foodstuff.errors[:price]).to include("価格は整数で入力してください")
+      expect(foodstuff.errors[:price]).to include(I18n.t('activerecord.errors.models.foodstuff.attributes.price.not_an_integer'))
     end
 
     it 'linkがない場合、無効である' do
       foodstuff = FactoryBot.build(:foodstuff, link: nil, user: user)
       expect(foodstuff).not_to be_valid
-      expect(foodstuff.errors[:link]).to include("リンクを入力してください")
+      expect(foodstuff.errors[:link]).to include(I18n.t('activerecord.errors.models.foodstuff.attributes.link.blank'))
     end
 
     it 'imageがない場合、無効である' do
       foodstuff = FactoryBot.build(:foodstuff, image: nil, user: user)
       expect(foodstuff).not_to be_valid
-      expect(foodstuff.errors[:image]).to include("画像をアップロードしてください")
+      expect(foodstuff.errors[:image]).to include(I18n.t('activerecord.errors.models.foodstuff.attributes.image.blank'))
     end
   end
 
@@ -56,20 +56,15 @@ RSpec.describe Foodstuff, type: :model do
 
   # 削除時のテスト
   describe '削除時のテスト' do
-    it 'userが削除されたときに関連するfoodstuffも削除される' do
-      foodstuff = FactoryBot.create(:foodstuff, user: user)
-      expect { user.destroy }.to change { Foodstuff.count }.by(-1)
-    end
-
     it 'foodstuffが削除されたときに関連するcommentsも削除される' do
       foodstuff = FactoryBot.create(:foodstuff, user: user)
-      comment = FactoryBot.create(:comment, user: user, commentable: foodstuff)
+      FactoryBot.create(:comment, user: user, commentable: foodstuff)
       expect { foodstuff.destroy }.to change { Comment.count }.by(-1)
     end
 
     it 'foodstuffが削除されたときに関連するuser_actionsも削除される' do
       foodstuff = FactoryBot.create(:foodstuff, user: user)
-      user_action = FactoryBot.create(:user_action, user: user, actionable: foodstuff)
+      FactoryBot.create(:user_action, user: user, actionable: foodstuff)
       expect { foodstuff.destroy }.to change { UserAction.count }.by(-1)
     end
   end
