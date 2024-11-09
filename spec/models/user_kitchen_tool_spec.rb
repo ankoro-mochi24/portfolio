db02c71a863fa3ee8553ff2fd5e6ca87430891b2
miyasaka <guleguleru@gmail.com>
2024-11-09 14:47:01 +0900

    モデルテスト-UserKitchenToolテーブル

--- spec/models/user_kitchen_tool_spec.rb
+++ spec/models/user_kitchen_tool_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe UserKitchenTool, type: :model do
   let(:user) { FactoryBot.create(:user) }
   let(:kitchen_tool) { FactoryBot.create(:kitchen_tool) }
 
+  before do
+    UserKitchenTool.delete_all
+    User.delete_all
+    KitchenTool.delete_all
+  end
+
   # バリデーションのテスト
   describe 'バリデーションのテスト' do
     it 'すべての属性が有効である場合、有効である' do
@@ -14,28 +20,48 @@ RSpec.describe UserKitchenTool, type: :model do
     it 'user_idがない場合、無効である' do
       user_kitchen_tool = FactoryBot.build(:user_kitchen_tool, user: nil, kitchen_tool: kitchen_tool, kitchen_tool_name: kitchen_tool.name)
       expect(user_kitchen_tool).not_to be_valid
-      expect(user_kitchen_tool.errors[:user]).to include("が必要です")
+      expect(user_kitchen_tool.errors[:user]).to include(I18n.t('errors.messages.required'))
     end
 
     it 'kitchen_tool_idがない場合、無効である' do
       user_kitchen_tool = FactoryBot.build(:user_kitchen_tool, user: user, kitchen_tool: nil, kitchen_tool_name: nil)
       expect(user_kitchen_tool).not_to be_valid
-      expect(user_kitchen_tool.errors[:kitchen_tool]).to include("が必要です")
+      expect(user_kitchen_tool.errors[:kitchen_tool]).to include(I18n.t('errors.messages.required'))
+    end
+
+    it 'kitchen_tool_nameがない場合、無効である' do
+      user_kitchen_tool = FactoryBot.build(:user_kitchen_tool, user: user, kitchen_tool_name: nil)
+      expect(user_kitchen_tool).not_to be_valid
+      expect(user_kitchen_tool.errors[:kitchen_tool_name]).to include(I18n.t('errors.messages.blank'))
     end
 
     it 'userとkitchen_toolの組み合わせが一意でない場合、無効である' do
       FactoryBot.create(:user_kitchen_tool, user: user, kitchen_tool: kitchen_tool, kitchen_tool_name: kitchen_tool.name)
       duplicate_user_kitchen_tool = FactoryBot.build(:user_kitchen_tool, user: user, kitchen_tool: kitchen_tool, kitchen_tool_name: kitchen_tool.name)
       expect(duplicate_user_kitchen_tool).not_to be_valid
-      expect(duplicate_user_kitchen_tool.errors[:user_id]).to include("はすでに存在します")
+      expect(duplicate_user_kitchen_tool.errors[:user_id]).to include(I18n.t('activerecord.errors.models.user_kitchen_tool.attributes.user_id.taken'))
+    end
+  end
+
+  # アソシエーションのテスト
+  describe 'アソシエーションのテスト' do
+    it 'ユーザーと調理器具に関連している' do
+      user_kitchen_tool = FactoryBot.build(:user_kitchen_tool, user: user, kitchen_tool: kitchen_tool)
+      expect(user_kitchen_tool.user).to eq(user)
+      expect(user_kitchen_tool.kitchen_tool).to eq(kitchen_tool)
     end
   end
 
   # 削除時のテスト
   describe '削除時のテスト' do
-    it 'userが削除されたときに関連するuser_kitchen_toolsも削除される' do
-      user_kitchen_tool = FactoryBot.create(:user_kitchen_tool, user: user, kitchen_tool: kitchen_tool, kitchen_tool_name: kitchen_tool.name)
+    it 'userが削除されたときに関連するuser_kitchen_toolも削除される' do
+      FactoryBot.create(:user_kitchen_tool, user: user, kitchen_tool: kitchen_tool, kitchen_tool_name: kitchen_tool.name)
       expect { user.destroy }.to change { UserKitchenTool.count }.by(-1)
+    end
+
+    it 'kitchen_toolが削除されたときに関連するuser_kitchen_toolも削除される' do
+      FactoryBot.create(:user_kitchen_tool, user: user, kitchen_tool: kitchen_tool, kitchen_tool_name: kitchen_tool.name)
+      expect { kitchen_tool.destroy }.to change { UserKitchenTool.count }.by(-1)
     end
   end
 
