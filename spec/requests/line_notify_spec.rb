6cc11baaa4ebd4ab6229b34abb3c6c5f6f3e8fb9
miyasaka <guleguleru@gmail.com>
2024-11-12 13:36:03 +0900

    リクエストテスト-line_notify_spec

--- spec/requests/line_notify_spec.rb
+++ spec/requests/line_notify_spec.rb
@@ -8,26 +8,25 @@ RSpec.describe LineNotifyController, type: :request do
   end
 
   describe "GET /line_notify/authorize" do
-    it "LINE Notify認証ページにリダイレクトされること" do
+    it "LINE Notify認証ページにリダイレクトされ、外部ホストのリダイレクトが許可されること" do
       get line_notify_authorize_path
+      
+      # リダイレクト先がLINE Notifyの認証ページであることを確認
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to(/https:\/\/notify-bot.line.me\/oauth\/authorize/)
+      
+      # 外部ホストのリダイレクトが許可されているかは、リダイレクト先URLのパターンで確認する
+      redirected_url = response.headers["Location"]
+      expect(redirected_url).to match(/https:\/\/notify-bot.line.me\/oauth\/authorize/)
     end
   end
 
   describe "GET /line_notify/callback" do
     context "有効なコールバックが受け取られた場合" do
-      let(:valid_params) do
-        {
-          code: 'valid_code',
-          state: user.id.to_s
-        }
-      end
+      let(:valid_params) { { code: 'valid_code', state: user.id.to_s } }
 
       it "トークンが保存され、プロフィールページにリダイレクトされること" do
-        response_body = {
-          access_token: 'valid_access_token'
-        }.to_json
+        response_body = { access_token: 'valid_access_token' }.to_json
 
         stub_request(:post, "https://notify-bot.line.me/oauth/token")
           .with(
@@ -46,19 +45,22 @@ RSpec.describe LineNotifyController, type: :request do
     end
 
     context "無効なコールバックが受け取られた場合" do
-      let(:invalid_params) do
-        {
-          code: nil,
-          state: user.id.to_s
-        }
-      end
+      let(:invalid_params) { { code: nil, state: user.id.to_s } }
 
       it "プロフィールページにエラーメッセージと共にリダイレクトされること" do
-        stub_request(:post, "https://notify-bot.line.me/oauth/token")
-          .to_return(status: 400, body: "", headers: {})
-
+        stub_request(:post, "https://notify-bot.line.me/oauth/token").to_return(status: 400, body: "", headers: {})
         get line_notify_callback_path, params: invalid_params
+        expect(response).to redirect_to(profile_path)
+        expect(flash[:alert]).to eq(I18n.t("errors.messages.line_notify_failed"))
+      end
+    end
 
+    context "LINE Notifyトークン取得リクエストで500エラーが発生した場合" do
+      let(:valid_params) { { code: 'valid_code', state: user.id.to_s } }
+
+      it "プロフィールページにエラーメッセージと共にリダイレクトされること" do
+        stub_request(:post, "https://notify-bot.line.me/oauth/token").to_return(status: 500, body: "", headers: {})
+        get line_notify_callback_path, params: valid_params
         expect(response).to redirect_to(profile_path)
         expect(flash[:alert]).to eq(I18n.t("errors.messages.line_notify_failed"))
       end
@@ -76,7 +78,6 @@ RSpec.describe LineNotifyController, type: :request do
         delete line_notify_unlink_path
         expect(response).to redirect_to(profile_path)
         expect(flash[:notice]).to eq(I18n.t("notices.line_notify_unlinked"))
-
         user.reload
         expect(user.line_notify_token).to be_nil
       end
