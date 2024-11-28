116cee21c9caa79766975342d89dce3f53686c88
miyasaka <guleguleru@gmail.com>
2024-11-28 11:08:28 +0900

    user_kitchen_tool_spec.rbの調整

--- app/models/user_kitchen_tool.rb
+++ app/models/user_kitchen_tool.rb
@@ -8,15 +8,20 @@
 # user(1)-(n)user_kitchen_tool(n)-(1)kitchen_tool
 class UserKitchenTool < ApplicationRecord
   belongs_to :user
-  belongs_to :kitchen_tool
+  belongs_to :kitchen_tool, optional: true
 
   attr_accessor :kitchen_tool_name
+
+  validates :user_id, uniqueness: { scope: :kitchen_tool_id }
 
   before_validation :set_kitchen_tool
 
   private
 
   def set_kitchen_tool
+    Rails.logger.debug "Before Validation: kitchen_tool_name=#{kitchen_tool_name.inspect}"
+    Rails.logger.debug "Before Validation: kitchen_tool=#{kitchen_tool.inspect}"
+
     if kitchen_tool_name.present?
       self.kitchen_tool = KitchenTool.find_or_create_by(name: kitchen_tool_name)
     else
