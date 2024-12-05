7d9282676b675cd8b45b2fe3105987cc0b726f44
miyasaka <guleguleru@gmail.com>
2024-12-05 14:09:51 +0900

    レシピ詳細のcontent_for設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def page_title(title = '')
     base_title = 'お米日和'
-    title.present? ? "#{base_title} | #{title}".html_safe : base_title
+    title.present? ? "#{base_title} | #{title}" : base_title
   end
 end
