3aa874b23af00d8743d408388d6fbe7893c0b1b5
miyasaka <guleguleru@gmail.com>
2024-12-18 14:07:29 +0900

    全体のデザイン調整

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -5,18 +7,16 @@ threads min_threads_count, max_threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 port ENV.fetch("PORT") { 3000 }
-
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+# PIDファイルの出力先を設定
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 before_fork do
   pid_file = File.join(Dir.pwd, 'tmp', 'pids', 'server.pid')
   if File.exist?(pid_file)
     File.delete(pid_file)
-    Rails.logger.debug "server.pid ファイルを削除しました"
-  else
-    Rails.logger.debug "server.pid ファイルは存在しません"
+    puts "server.pid ファイルを削除しました"
   end
 end
 
